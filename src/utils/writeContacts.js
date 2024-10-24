5ca474430aa06cfda3f0ee6eacae8a6b51226779
Irina Babenko <babenko8irina@gmail.com>
2024-10-25 00:53:01 +0300

    Initial project structure and dependencies

--- src/utils/writeContacts.js
+++
@@ -1,6 +0,0 @@
-import { writeFile } from 'node:fs/promises';
-
-import { PATH_DB } from '../constants/contacts.js';
-
-export const writeContacts = (contacts) =>
-  writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
