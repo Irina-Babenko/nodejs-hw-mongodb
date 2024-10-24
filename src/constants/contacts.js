897b95100360fdcd09236071b215f1e9793abb58
Irina Babenko <159971696+Irina-Babenko@users.noreply.github.com>
2024-10-25 00:06:45 +0300

    Initial commit

---
+++ src/constants/contacts.js
@@ -0,0 +1,9 @@
+import * as path from 'node:path';
+
+// const contactPath = path.join(process.cwd(), 'src', 'db', 'db.json');
+// console.log(contactPath);
+
+// const contactPath = path.resolve('src', 'db', 'db.json');
+// console.log(contactPath);
+
+export const PATH_DB = path.join(process.cwd(), 'src', 'db', 'db.json');
