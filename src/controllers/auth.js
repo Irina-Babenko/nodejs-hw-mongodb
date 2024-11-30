38d820b89ba87abb8973bb11bc5cbebb88433ac8
Irina Babenko <babenko8irina@gmail.com>
2024-11-30 03:01:55 +0200

    add rout send-reset-email and rout reset-pwd

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,6 +1,10 @@
-import createHttpError from 'http-errors';
+// import createHttpError from 'http-errors';
 
 import * as authServices from '../services/auth.js';
+
+import { requestResetToken } from '../services/auth.js';
+
+import { resetPassword } from '../services/auth.js';
 
 const setupSession = (res, session) => {
   const { _id, refreshToken, refreshTokenValidUntil } = session;
@@ -67,4 +71,24 @@ export const logoutController = async (req, res) => {
   res.clearCookie('refreshToken');
 
   res.status(204).send();
+};
+
+export const requestResetEmailController = async (req, res) => {
+  await requestResetToken(req.body.email);
+
+  res.json({
+    message: 'Reset password email was successful sent!',
+    status: 200,
+    data: {},
+  });
+};
+
+export const resetPasswordController = async (req, res) => {
+  await resetPassword(req.body);
+
+  res.json({
+    status: 200,
+    message: 'Password has been successfully reset.',
+    data: {},
+  });
 };
--- src/validation/auth.js
+++ src/validation/auth.js
@@ -14,3 +14,14 @@ export const authLoginSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().min(6).required(),
 });
+
+//
+export const requestResetEmailSchema = Joi.object({
+  email: Joi.string().email().required(),
+});
+
+//
+export const resetPasswordSchema = Joi.object({
+  password: Joi.string().required(),
+  token: Joi.string().required(),
+});
